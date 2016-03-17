d700b31e047263f10e543555604981492f283692
Thomas Gerbet <thomas.gerbet@enalean.com>
2016-03-17 10:44:00 +0100

    Display an error message when the setup script takes too much time

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ createrepo /rpms/RPMS/noarch/
 yum install -y --disableexcludes=Tuleap tuleap-documentation && yum install -y --enablerepo=rpmforge-extras tuleap-all
 
 timeout 2m /usr/share/tuleap-install/setup.sh --disable-selinux --sys-default-domain='tuleap.test' --sys-org-name='TuleapTest' --sys-long-org-name='TuleapTest'
+if [ "$?" -eq 124 ]; then
+    >&2 echo "The setup script takes too much time and has been killed. It might be because you expect a user interaction."
+fi
