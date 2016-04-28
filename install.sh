550abcd14dc6142d89590418ea8daa1b6674d2f1
tty <matthieu@beastmail.org>
2016-04-28 14:49:49 +0200

    Disable domain name check (#2)

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ createrepo /rpms/RPMS/noarch/
 
 yum install -y --disableexcludes=Tuleap tuleap-documentation && yum install -y --enablerepo=rpmforge-extras tuleap-all
 
-timeout 2m /usr/share/tuleap-install/setup.sh --disable-selinux --sys-default-domain='tuleap.test' --sys-org-name='TuleapTest' --sys-long-org-name='TuleapTest'
+timeout 2m /usr/share/tuleap-install/setup.sh --disable-domain-name-check --disable-selinux --sys-default-domain='tuleap.test' --sys-org-name='TuleapTest' --sys-long-org-name='TuleapTest'
 if [ "$?" -eq 124 ]; then
     >&2 echo "The setup script takes too much time and has been killed. It might be because you expect a user interaction."
 fi
