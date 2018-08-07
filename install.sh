1610a302c3aa461dadc91798e19b624b20005088
Thomas Gerbet <thomas.gerbet@enalean.com>
2018-08-07 18:01:02 +0200

    Install the Git plugin using Gitolite3

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ RPM_PATH="/rpms";
 
 createrepo /rpms/RPMS/noarch/
 
-yum install -y --disableexcludes=Tuleap tuleap-documentation && yum install -y tuleap-all
+yum install -y --disableexcludes=Tuleap tuleap-documentation && yum install -y tuleap-all tuleap-plugin-git-gitolite3
 
 /usr/share/tuleap-install/setup.sh --disable-domain-name-check --sys-default-domain='tuleap.test' --sys-org-name='TuleapTest' --sys-long-org-name='TuleapTest'
