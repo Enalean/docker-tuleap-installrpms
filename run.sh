27adfd43d6ecf7da34a13435fe2b8fbe596f959b
Manuel VACELET <manuel.vacelet@enalean.com>
2019-10-08 15:51:00 +0200

    Avoid race conditions at stack start
    It might happen that fpm is not quite ready to server requests
    right after tuleap start, give it a bit of time to complete
    the startup sequence before fetching the page

--- run.sh
+++ run.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
 /install.sh
 
-timeout 20s systemctl start tuleap
-curl --insecure --output /output/index.html https://127.0.0.1/
+systemctl start tuleap
+
+count=0
+code=$(curl --insecure --silent --output /dev/null --write-out "%{http_code}"  https://127.0.0.1 || true)
+while [ $code -ne 200 ] && [ $count -ne 10 ]; do
+  echo "`date` Tuleap not ready ($code) attempt #$count"
+  sleep $((count+1))
+  code=$(curl --insecure --silent --output /dev/null --write-out "%{http_code}" https://127.0.0.1 || true)
+  count=$((count+1))
+done
+
+curl --silent --insecure --output /output/index.html https://127.0.0.1/
 
 timeout 20s journalctl --no-pager > /output/journalctl.log
