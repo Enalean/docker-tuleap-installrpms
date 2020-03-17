9665bf7930cec643203fc6bf73ca411594d3c1bc
Manuel VACELET <manuel.vacelet@enalean.com>
2020-03-17 16:50:56 +0100

    Install C6 with Mysql 5.7 / CI

--- Dockerfile
+++ Dockerfile
@@ -8,10 +8,16 @@ RUN yum install -y openssh-server \
     centos-release-scl \
     curl \
     https://rpms.remirepo.net/enterprise/remi-release-6.rpm && \
-    yum install -y --disableexcludes=Tuleap tuleap-documentation && \
+    yum install -y --disableexcludes=Tuleap \
+        tuleap-documentation \
+        rh-mysql57-mysql \
+        rh-mysql57-mysql-server \
+	&& \
     yum clean all
 
 COPY tuleap-local.repo /etc/yum.repos.d/
+
+COPY mysql-server.cnf /etc/opt/rh/rh-mysql57/my.cnf.d/mysql-server.cnf
 
 COPY install.sh /install.sh
 COPY run.sh /run.sh
--- install.sh
+++ install.sh
@@ -4,7 +4,15 @@ createrepo /rpms/RPMS/noarch/
 
 yum install -y tuleap-all tuleap-plugin-git-gitolite3
 
-timeout 2m /usr/share/tuleap-install/setup.sh --disable-domain-name-check --sys-default-domain='tuleap.test' --sys-org-name='TuleapTest' --sys-long-org-name='TuleapTest'
+if [ "${DB}" = "mysql57" ]; then
+    service rh-mysql57-mysqld start
+    scl enable rh-mysql57 -- mysqladmin -uroot password welcome0
+
+    timeout 2m //usr/share/tuleap-install/setup.sh --disable-domain-name-check --sys-default-domain='tuleap.test' --sys-org-name='TuleapTest' --sys-long-org-name='TuleapTest' --mysql-host=localhost --mysql-user=root --mysql-user-password=welcome0
+else
+    timeout 2m /usr/share/tuleap-install/setup.sh --disable-domain-name-check --sys-default-domain='tuleap.test' --sys-org-name='TuleapTest' --sys-long-org-name='TuleapTest'
+fi
+
 if [ "$?" -eq 124 ]; then
     >&2 echo "The setup script takes too much time and has been killed. It might be because you expect a user interaction."
 fi
