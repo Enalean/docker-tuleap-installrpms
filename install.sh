9b1c0f0249421222c7232ce90959d957c4f4b0cf
Manuel VACELET <manuel.vacelet@enalean.com>
2020-03-17 16:41:59 +0100

    Install with mysql57

--- install.sh
+++ install.sh
@@ -5,4 +5,11 @@ createrepo /rpms/RPMS/noarch/
 
 yum install -y --disableexcludes=Tuleap tuleap-documentation && yum install -y tuleap-all tuleap-plugin-git-gitolite3
 
-/usr/share/tuleap-install/setup.sh --disable-domain-name-check --sys-default-domain='tuleap.test' --sys-org-name='TuleapTest' --sys-long-org-name='TuleapTest'
+if [ "${DB}" = "mysql57" ]; then
+    service rh-mysql57-mysqld start
+    scl enable rh-mysql57 -- mysqladmin -uroot password welcome0
+
+    /usr/share/tuleap-install/setup.sh --disable-domain-name-check --sys-default-domain='tuleap.test' --sys-org-name='TuleapTest' --sys-long-org-name='TuleapTest' --mysql-host=localhost --mysql-user=root --mysql-user-password=welcome0
+else
+    /usr/share/tuleap-install/setup.sh --disable-domain-name-check --sys-default-domain='tuleap.test' --sys-org-name='TuleapTest' --sys-long-org-name='TuleapTest'
+fi
